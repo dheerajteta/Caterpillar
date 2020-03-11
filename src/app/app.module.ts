821f23e44b86921a66cc2be1adf1204ffeb17a1b
Dheeraj Teta <dheeraj.teta@rksv.in>
2020-03-11 22:09:09 +0530

    [comp1]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Comp1Component } from './components/comp1/comp1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp1Component
   ],
   imports: [
     BrowserModule,
